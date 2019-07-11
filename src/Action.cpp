1d1e2d29a93195b5ba7e94fff0f278268a2e4cfe
Daniel Nyström <sp2danny@gmail.com>
2019-07-11 16:50:28 +0200

    key shortcut

--- src/Action.cpp
+++ src/Action.cpp
@@ -1,25 +1,32 @@
 
 #include "Action.h"
 
-//	std::string name;
-//	bool        haveButton;
-//	bool        haveShortcut;
-//	bool        dynamicLocation;
-//	bool        visible;
-//	bool        enabled;
-//	int         location;
-//	int         x, y;
-//	alib::AD    button;
-//	std::string tooltip;
-//	char        shortcut;
-//	std::function<void(Ref<Action>)> onActivate;
-
 namespace C35 {
 
 void RunAction(Ref<Action> a)
 {
 	std::cout << a->name() << std::endl;
 }
+
+void AddAction(alib::AC& ua, std::string name, std::string anim, std::string tt, int location, char shortcut)
+{
+	int id = Action::create();
+	Action& a = *Action::lookup(id);
+	a.name() = name;
+	a.haveButton = true;
+	a.haveShortcut = true;
+	a.dynamicLocation = true;
+	a.location = location;
+	a.x = a.y = 0;
+	a.button.AddDir(0, ua.Get(anim).Get(0).Get(0));
+	a.button.AddDir(1, ua.Get(anim+"HL").Get(0).Get(0));
+	a.button.Instance(0);
+	a.tooltip = tt;
+	a.shortcut = shortcut;
+	a.onActivate = &RunAction;
+}
+
+} // namesapce C35
 
 void C35::Action::AllOff()
 {
@@ -48,7 +55,15 @@ bool C35::Action::ParseInput(sf::Event& e)
 		if (p.y > (r.top+r.height)) return false;
 		return true;
 	};
-
+	
+	if (e.type == sf::Event::KeyPressed)
+	{
+		if (haveShortcut && e.key.code == shortcut)
+		{
+			onActivate(ref());
+			return true;			
+		}
+	}
 	if (e.type == sf::Event::MouseMoved)
 	{
 		if (inside(e.mouseMove, rect))
@@ -63,7 +78,7 @@ bool C35::Action::ParseInput(sf::Event& e)
 	{
 		if (inside(e.mouseButton, rect))
 		{
-			onActivate(this->ref());
+			onActivate(ref());
 			return true;
 		}
 	}
@@ -81,26 +96,6 @@ void C35::Action::Display(sf::RenderWindow& rw)
 bool C35::Action::Done()
 {
 	return false;
-}
-
-void AddAction(alib::AC& ua, std::string name, std::string anim, std::string tt, int location, char shortcut)
-{
-	int id = Action::create();
-	Action& a = *Action::lookup(id);
-	a.name() = name;
-	a.haveButton = true;
-	a.haveShortcut = true;
-	a.dynamicLocation = true;
-	a.location = location;
-	a.x = a.y = 0;
-	a.button.AddDir(0, ua.Get(anim).Get(0).Get(0));
-	a.button.AddDir(1, ua.Get(anim+"HL").Get(0).Get(0));
-	a.button.Instance(0);
-	a.tooltip = tt;
-	a.shortcut = shortcut;
-	a.onActivate = &RunAction;
-}
-
 }
 
 void C35::MakeActions()
